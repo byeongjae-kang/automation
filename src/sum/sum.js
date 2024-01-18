09ef5708fa193312f910a243eacf6a16969b84fb
Bart Kang <bart.kang.0719@gmail.com>
2024-01-18 11:28:51 -0800

    polish

--- src/sum/sum.js
+++ src/sum/sum.js
@@ -1,4 +1,8 @@
 export const sum = (...rest) => {
+  if (rest.length > 10) {
+    return rest.reduce((prev, curr) => prev + curr, 0);
+  }
+
   for (let i = 0; i < rest.length; i++) {
     if (isNaN(rest[i])) {
       throw new Error("hey this is not a number which is not acceptable");
--- src/sum/sum.spec.js
+++ src/sum/sum.spec.js
@@ -2,7 +2,7 @@ import { sum } from "./sum.js";
 
 describe("sum function", () => {
   it("should sum up 1 and 1 and return 2", () => {
-    expect(sum(1, 1)).toBe(3);
+    expect(sum(1, 1)).toBe(2);
   });
 
   it("should throw an error when passed arg is not number", () => {
@@ -11,5 +11,6 @@ describe("sum function", () => {
 
   it("should be able handle long number decimal and return two decimal places", () => {
     expect(sum(1.192379182, 2.128379)).toBe(3.32);
+    expect(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)).toBe(66);
   });
 });
